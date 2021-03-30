a244c9095f830de0734d83343524fa17938f871f
Sergdml <sergio.diaz-maroto@alumnos.upm.es>
2021-03-30 21:33:05 +0200

    Creado base menu
    Falta por añadir las reglas.

--- MENU PROVISIONAL/Menú provisional.c
+++ MENU PROVISIONAL/Menú provisional.c
@@ -1,1 +1,35 @@
 #include<stdio.h>
+
+int main()
+{
+    char opcion;
+    printf("\n****************BIENVENIDO AL JUEGO DEL DADO MENTIROSO****************\n");
+    do
+    {
+        printf("\n  A continuacion se mostraran multiples opciones, elija la opcion que desee pulsando dicha tecla:\n");
+        printf("\tA-->Si desea conocer las reglas del juego\n\tB-->Si desea jugar contra otro jugador\n\tC-->Si desea jugar contra una IA\n\tD-->Si desea salir del menu\n");
+        scanf(" %c",&opcion);
+    switch (opcion)
+    {
+
+       case 'A':
+       case 'a':
+           printf("Reglas \n");
+           break;
+
+       case 'B':
+       case 'b':
+           printf("opcion B \n");
+           break;
+
+       case 'C':
+       case 'c':
+           printf("opcion C\n");
+           break;
+
+    }
+    }
+    while((opcion=='A')||(opcion=='B')||(opcion=='a')||(opcion=='b')||(opcion=='C')||(opcion=='c'));
+
+    return 0;
+}
