856dc59bebfadcef4dd77c5c546e5e73d4c83bf8
Sergdml <sergio.diaz-maroto@alumnos.upm.es>
2021-03-31 17:32:52 +0200

    Retocado menu provisional
    Faltan añadir las reglas en la opción A.

--- MENU PROVISIONAL/Menú provisional.c
+++ MENU PROVISIONAL/Menú provisional.c
@@ -2,34 +2,54 @@
 
 int main()
 {
-    char opcion;
-    printf("\n****************BIENVENIDO AL JUEGO DEL DADO MENTIROSO****************\n");
+    char opcion,volver[7];
+    printf("\n********************|| BIENVENIDO AL JUEGO DEL DADO MENTIROSO ||********************\n");
     do
     {
-        printf("\n  A continuacion se mostraran multiples opciones, elija la opcion que desee pulsando dicha tecla:\n");
-        printf("\tA-->Si desea conocer las reglas del juego\n\tB-->Si desea jugar contra otro jugador\n\tC-->Si desea jugar contra una IA\n\tD-->Si desea salir del menu\n");
+        printf("\n\n  A continuacion se mostraran multiples opciones.\n  Elija la opcion que desee pulsando dicha tecla y posteriormente 'enter':\n");
+        printf("\n");
+        printf("\tA --> Reglas del juego DADO MENTIROSO.\n\n\tB --> Jugar contra otro jugador.\n\n\tC --> Jugar contra una IA.\n\n\tD --> Salir del menu.\n\n\n\n");
         scanf(" %c",&opcion);
     switch (opcion)
     {
 
        case 'A':
        case 'a':
-           printf("Reglas \n");
+           printf("opcion A \n");//Faltan por añadir las normas aqui.
+           printf("\n-----| Escriba 'volver' y posteriormente pulse 'enter' para volver al menu anterior |-----\n\n");
+           scanf(" %6s", volver);//Una forma un poco "casera" de hacer esperar al programa hasta que la persona pulse "una tecla" (en este caso mediante una palabra).
            break;
 
        case 'B':
        case 'b':
            printf("opcion B \n");
+           printf("\n-----| Escriba 'volver' y posteriormente pulse 'enter' para volver al menu anterior |-----\n\n");
+           scanf(" %6s", volver);
            break;
 
        case 'C':
        case 'c':
            printf("opcion C\n");
+           printf("\n-----| Escriba 'volver' y posteriormente pulse 'enter' para volver al menu anterior |-----\n\n");
+           scanf(" %6s", volver);
            break;
+
+       case 'D':
+       case 'd':
+        printf("*************************************| Hasta luego!! |*************************************\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~| Esperemos verle pronto por aqui |~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+
+        break;
+
+       default://Por si la persona se equivoca de tecla para que el menu siga abierto.
+        printf("\n--------------------------|  Esta opcion no esta contemplada  |---------------------------\n");
+        printf("-----| Escriba 'volver' y posteriormente pulse 'enter' para volver al menu anterior |-----\n\n");
+        scanf(" %6s", volver);
+        break;
+
 
     }
     }
-    while((opcion=='A')||(opcion=='B')||(opcion=='a')||(opcion=='b')||(opcion=='C')||(opcion=='c'));
+    while((opcion!='d')&&(opcion!='D'));
 
     return 0;
 }
