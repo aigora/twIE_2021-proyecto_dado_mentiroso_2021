2fcd24739d3bf1178054885cbafccea7639487eb
Sergdml <sergio.diaz-maroto@alumnos.upm.es>
2021-05-10 13:17:42 +0200

    Reorganización y creación de nuevas librerias.
    Reorganización para que se haga mas fácil la implementación del elemento gráfico al proyecto.

--- 0- CÓDIGO PROYECTO/MAIN.c
+++ 0- CÓDIGO PROYECTO/MAIN.c
@@ -1,8 +1,8 @@
-/**<
+
 #include <time.h>
 #include <stdlib.h>
 #include <math.h>
-#include "libfuncion-1.h"
+#include "libfuncion-JUEGO.h"
 #define U 0.036 //Umbral
 
 
@@ -49,108 +49,7 @@ Si el acusador tuvo razon, gana la partida, pero si se da el caso de que el acus
        case 'c':
            printf("Jugar contra una IA\n\n");
 
-
-
-            int i;
-            int dados_jugador1[5];
-            int dados_jugador2[5];
-            int contador_dados_IA[6] = {0};
-            int num_dado_apostado;
-            int cantidad_apostada;
-            int cantidad_de_la_IA_del_dadoapostado; //si yo, usuario, he apostado un 5, la maquina utiliza esta variable para saber cuantos 5 tiene
-            float Prop_exito = 1.0/6.0;
-            float Prop_fracaso = 5.0/6.0;
-            float Binomial;
-            int c;
-            int n = 5;
-
-            //Genera semilla aleatoria
-            srand(time(NULL));
-
-            for(i = 0; i < 5; i++)
-                dados_jugador1[i] = randomizer();
-
-            for(i = 0; i < 5; i++)
-                dados_jugador2[i] = randomizer();
-
-            printf("Los dados con los qque usted jugara seran:\n");
-            printf("\n");
-
-            imprime(dados_jugador1, 5);
-            printf("\n\n");
-
-
-            //Este bucle contabiliza en un nuevo vector(contador_dados_IA) la cantidad de cada numero de dados que le ha tocado a la IA
-            for(i = 0; i < 5; i++)
-            {
-            switch(dados_jugador2[i])
-                {
-                case 1:
-                    contador_dados_IA[0] += 1;
-                    break;
-                case 2:
-                    contador_dados_IA[1] += 1;
-                    break;
-                case 3:
-                    contador_dados_IA[2] += 1;
-                    break;
-                case 4:
-                    contador_dados_IA[3] += 1;
-                    break;
-                case 5:
-                    contador_dados_IA[4] += 1;
-                    break;
-                case 6:
-                    contador_dados_IA[5] += 1;
-                    break;
-                }
-            }
-            //imprime(contador_dados_IA, 6);
-            printf("\n\n");
-
-            do
-            {
-                printf("\nEscribe la cantidad que apuestas\n");
-                printf("(Debe estar comprendido entre 1 y 10).\n");
-                printf("\n");
-                scanf(" %i", &cantidad_apostada);
-            }while ((cantidad_apostada < 1) || (cantidad_apostada > 10));
-
-            do
-            {
-                printf("\nEscribe el numero de dado que apuestas\n");
-                printf("(Debe estar comprendido entre 1 y 6).\n");
-                printf("\n");
-                scanf(" %i", &num_dado_apostado);
-            }while ((num_dado_apostado < 1) || (num_dado_apostado > 6));
-
-            cantidad_de_la_IA_del_dadoapostado = identificador(contador_dados_IA, 6, num_dado_apostado);
-            printf("\n\n");
-            //printf("%i", cantidad_de_la_IA_del_dadoapostado);
-            printf("\n\n");
-
-
-            c = cantidad_apostada - cantidad_de_la_IA_del_dadoapostado;
-            //Si se da el caso de que c sigue siendo mayor que 5, se usa esto para volver a una binomial entre 10 dados
-            if(c >= 5)
-            {
-                c = cantidad_apostada;
-                n = 10;
-            }
-
-            for (i = c; i < n; i++)
-            {
-                Binomial += Prop_binomial(Prop_exito, Prop_fracaso, i, n);
-            }
-            printf("La probabilidad de que la apuesta realizada exista es de:\n");
-            printf("%f\n", Binomial);
-
-            if(Binomial < U)
-            {
-                printf("Mentiroso!!\n");
-            }
-            imprime(dados_jugador2, 5);
-
+           JUGADORIA();
 
            printf("\n\n-----| Escriba 'volver' y posteriormente pulse 'enter' para volver al menu anterior |-----\n\n");
            scanf(" %6s", volver);
@@ -175,4 +74,4 @@ Si el acusador tuvo razon, gana la partida, pero si se da el caso de que el acus
 
     return 0;
 }
-*/
+
