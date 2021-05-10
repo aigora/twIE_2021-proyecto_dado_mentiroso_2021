bce52ff236018691bcdc423bd858d001fec71568
rubencasstro <ruben.castro@alumnos.upm.es>
2021-05-10 18:56:55 +0200

    implementación de fichero para mostrar las reglas

--- 0- CÓDIGO PROYECTO/MAIN.c
+++ 0- CÓDIGO PROYECTO/MAIN.c
@@ -9,7 +9,7 @@
 
 int main()
 {
-    char opcion,volver[7];
+    char opcion,caracter[100],volver[7];
     printf("\n********************|| BIENVENIDO AL JUEGO DEL DADO MENTIROSO ||********************\n");
     do
     {
@@ -18,23 +18,21 @@ int main()
         printf("\tA --> Explicacion del juego DADO MENTIROSO.\n\n\tB --> Jugar contra otro jugador.\n\n\tC --> Jugar contra una IA.\n\n\tD --> Salir del menu.\n\n\n\n");
         scanf(" %c",&opcion);
         printf("\n\n");
-
+        FILE *archivo;
     switch (opcion)
     {
 
        case 'A':
        case 'a':
-           printf("\nEl juego del dado mentiroso es un juego de apuestas donde cada jugador tiene 5 dados y un cubilete.\n\
-Al comenzar, todos los jugadores agitan los dados dentro del cubilete y lo posan sobre la mesa boca abajo\n\
-para no revelar tus dados a los demas jugadores. Un jugador puede mirar sus dados, pero no los de los demas.\n\
-El juego consiste en apostar una cantidad de dados especifica de un numero, es decir, si es un juego de dos personas,\n\
-tu puedes decir que hay cuatro dados con el numero tres entre los diez dados totales.\n\
-El juego es en si un bucle de apuestas cada vez mas grandes hasta llegar a un suceso muy improbable.\n\
-Cuando un jugador considere que la apuesta realizada por el jugador anterior es falsa,\n\
-este podra llamarle mentiroso y se tendran que revelar los dados de todos los jugadores.\n\
-Si el acusador tuvo razon, gana la partida, pero si se da el caso de que el acusador no tuvo razon al llamarle mentiroso\
-(porque si hay de verdad dicha cantidad de dados) el acusador pierde.\n\
-\nY eso es todo, mucha suerte!!\n");//Se han añadido mediante un printf simple, posteriormente se añadiran como fichero.
+           archivo = fopen("reglas_dado_mentiroso.txt","r");
+
+ 	    while (feof(archivo) == 0)
+ 	    {
+ 		fgets(caracter,100,archivo);
+ 		printf("%s",caracter);
+ 	    }
+        fclose(archivo);
+
            printf("\n\n-----| Escriba 'volver' y posteriormente pulse 'enter' para volver al menu anterior |-----\n\n");
            scanf(" %6s", volver);//Una forma un poco "casera" de hacer esperar al programa hasta que la persona pulse "una tecla" (en este caso mediante una palabra).
            break;
